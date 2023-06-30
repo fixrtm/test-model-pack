73cfc8838a016f86e38f4dd34e27974d63af0245
anatawa12 <anatawa12@icloud.com>
2023-06-30 13:15:20 +0900

    chore: make it easier to add showing information

--- assets/minecraft/scripts/fixrtm_debugtrain.js
+++ assets/minecraft/scripts/fixrtm_debugtrain.js
@@ -24,10 +24,12 @@ function render(entity, pass, par3)
         GL11.glTranslatef(0, 3, 0);
         GL11.glRotatef(-renderer.getYaw(entity), 0, 1, 0);
         GL11.glScalef(2, 2, 2);
-        renderText("f-dir: " + entity.formation.getEntry(entity).dir, 0, 0, 0, -30);
-        renderText("dor: " + entity.getVehicleState(TrainStateType.Door), 0, 0, 0, -20);
-        renderText("dir: " + entity.getVehicleState(TrainStateType.Direction), 0, 0, 0, -10);
-        renderText("spd: " + entity.getSpeed() * METER_PER_TICK_TO_KILO_METER_PER_HOUR, 0, 0, 0, 0);
+        renderTexts([
+            "f-dir: " + entity.formation.getEntry(entity).dir, 0,
+            "dor: " + entity.getVehicleState(TrainStateType.Door), 0,
+            "dir: " + entity.getVehicleState(TrainStateType.Direction), 0,
+            "spd: " + entity.getSpeed() * METER_PER_TICK_TO_KILO_METER_PER_HOUR,
+        ], 0, 0, 0)
         GL11.glPopMatrix();
     }
 
@@ -39,7 +41,7 @@ var EntityRenderer = Packages.net.minecraft.client.renderer.EntityRenderer;
 var minecraft = Minecraft[getName("func_71410_x", "getMinecraft")]();
 //var minecraft = Minecraft.getMinecraft();
 
-function renderText(text, x, y, z, verticalShift) {
+function renderTexts(texts, x, y, z) {
     var fontRenderer = minecraft[getName("field_71466_p", "fontRenderer")];
     var renderManager = minecraft[getName("func_175598_ae", "getRenderManager")]();
     var viewerYaw = renderManager[getName("field_78735_i", "playerViewY")];
@@ -47,8 +49,14 @@ function renderText(text, x, y, z, verticalShift) {
     var isThirdPersonFrontal = renderManager[getName("field_78733_k", "options")][getName("field_74320_O", "thirdPersonView")] === 2;
     var isSneaking = false;
 
-    EntityRenderer[getName("func_189692_a", "drawNameplate")](fontRenderer, text, x, y, z,
-        verticalShift, viewerYaw, viewerPitch, isThirdPersonFrontal, isSneaking)
+    var verticalShift = (texts.length - 1) * -10;
+
+    for (var i = 0; i < texts.length; i++, verticalShift += 10) {
+        var text = texts[i]
+
+        EntityRenderer[getName("func_189692_a", "drawNameplate")](fontRenderer, text, x, y, z,
+            verticalShift, viewerYaw, viewerPitch, isThirdPersonFrontal, isSneaking)
+    }
 }
 
 var cache;
